69607feff2d6d574f989d8c2b414649753ee7e19
Molly Foster <mollyrfoster@gmail.com>
2017-05-29 13:51:58 -0600

    search functionality

--- script.js
+++ script.js
@@ -24,10 +24,10 @@ $('.save-button').on('click', function() {
 });
 
 //Up-Vote Event
-$('.idea-container').on('click', '.up-vote', upvote);
+$('.idea-container').on('click', '.up-vote', upvoteStorage);
 
 //Down-Vote Event
-$('.idea-container').on('click', '.down-vote', downvote);
+$('.idea-container').on('click', '.down-vote', downvoteStorage);
 
 //Delete idea cards from DOM and localStorage
 $('.idea-container').on('click', '.delete', function() {
@@ -62,20 +62,8 @@ function buildNewCard (title, body, quality, id){
   var ideaTitle = $('.title-input').val() || title;
   var ideaBody = $('.body-input').val() || body;
   // var ideaId = id;
-  var ideaQuality =  quality || "swill"
-  // console.log(parseExisting);
-  // var ideaID = Date.now();
-  // if ( localStorage.getItem('ideaArray') ) {
-  //
-  //   parseBlah.forEach(function(element) {;
-  //     console.log(element);
-  //     if(ideaId == element.id){
-  //       ideaQuality = element.quality
-  //     }
-  //   });
-  // } else {
-  //   ideaQuality = "swill";
-  // }
+  var ideaQuality = quality || "swill"
+
   var newIdea = new IdeaConstructor(ideaTitle, ideaBody, ideaQuality);
   $('.idea-container').prepend(`
       <article class="idea-card" id="${newIdea.id}">
@@ -115,102 +103,108 @@ function getIdeasFromStorage () {
 
 //upvote button function :: not functional
 function upvote() {
-  var qualityInput = $(this).closest('.idea-card').find('#vote');
+  var qualityInput = $(event.target).closest('.idea-card').find('#vote');
     if (qualityInput.text() === 'swill') {
       qualityInput.text('plausible');
     } else if (qualityInput.text() === 'plausible'){
       qualityInput.text('genius')
     }
+}
 
+function upvoteStorage() {
+  upvote();
+  var qualityInput = $(event.target).closest('.idea-card').find('#vote');
   var thisId = $(this).parent().parent().prop('id');
   var parsedQuality = JSON.parse(localStorage.getItem('ideaArray'));
-          // console.log(parsedQuality);
-  parsedQuality.forEach(function(element) {
-    if (thisId == element.id) {
+    parsedQuality.forEach(function(element) {
+      if (thisId == element.id) {
       element.quality = qualityInput.text();
-      // console.log(element);
-    }
-    // parsedQuality = element.quality;
-  });
-        console.log(parsedQuality);
+      }
+    });
   ideaArray = parsedQuality;
-            console.log(ideaArray);
-
   addToLocal();
-
-    // console.log(localStorage)
-
-  // ideaArray.push(parsedQuality);
-  // console.log(ideaArray);
-  // localStorage.setItem('ideaArray', JSON.stringify(ideaArray));
 }
 
-
-
-
-
-  // storeQuality();
-
-
-// function storeQuality () {
-//   // var id = $('.up-vote').parents('.idea-card')[0].id;
-//   var qualityInput = $('.up-vote').closest('.idea-card').find('#vote');
-//   console.log(qualityInput);
-//   var id = $('.up-vote').closest('.idea-card').prop('id');
-//   console.log(id);
-//   var parsedQuality = JSON.parse(localStorage.getItem('ideaArray'));
-//   console.log(ideaArray.id);
-//   if (id == parsedQuality.id) {
-//     parsedQuality.forEach(function(element) {
-//     element.quality = qualityInput;
-//     console.log(element.quality);
-//     parsedQuality = element.quality;
-//     console.log(parsedQuality);
-//   })
-// }
-// console.log(parsedQuality);
-// // ideaArray.push(parsedQuality);
-//
-//   // localStorage.setItem('ideaArray', JSON.stringify(ideaArray));
-// }
-
-//downvote button function :: not functional
+//downvote button
 function downvote() {
-  var qualityInput = $(this).closest('.idea-card').find('#vote')
-  if (qualityInput.text() === 'genius'){
-    qualityInput.text('plausible')
-  }else if (qualityInput.text() === 'plausible') {
-    qualityInput.text('swill')
-  }
+  var qualityInput = $(event.target).closest('.idea-card').find('#vote');
+    if (qualityInput.text() === 'genius') {
+      qualityInput.text('plausible');
+    } else if (qualityInput.text() === 'plausible'){
+      qualityInput.text('swill')
+    }
 }
 
-$('.search-input').on('input', search)
-
-function search() {
-  searchTitle();
-  searchBody();
+function downvoteStorage() {
+  downvote();
+  var qualityInput = $(event.target).closest('.idea-card').find('#vote');
+  var thisId = $(this).parent().parent().prop('id');
+  var parsedQuality = JSON.parse(localStorage.getItem('ideaArray'));
+    parsedQuality.forEach(function(element) {
+      if (thisId == element.id) {
+      element.quality = qualityInput.text();
+      }
+    });
+  ideaArray = parsedQuality;
+  addToLocal();
 }
-//Search Bar Function
-function searchTitle() {
-  var inputText = $('.search-input').val();
-  console.log(inputText)
-  if (inputText) {
-    $('.idea-card').find("h2:not(:contains(" + inputText + "))").closest('.idea-card').slideUp();
-    $('.idea-card').find("h2:contains(" + inputText + ")").slideDown()
-} else {
-  $('.idea-card').slideDown();
-  }
-};
 
-function searchBody() {
-    var inputText = $('.search-input').val();
-    if (inputText) {
-    $('.idea-card').find(".idea-text:not(:contains(" + inputText + "))").closest('.idea-card').slideUp();
-    $('.idea-card').find(".idea-text:contains(" + inputText + ")").slideDown();
-  } else {
-    $('.idea-card').slideDown();
-  }
-};
+
+
+function prependExistingCard(filtered) {
+  $('.idea-container').prepend(`
+      <article class="idea-card" id="${filtered.id}">
+        <div class="card-top">
+          <h2 id="to-do-title" contenteditable="true">${filtered.title}</h2>
+          <button class="delete icon"></button>
+        </div>
+        <p contenteditable="true" class="idea-text">${filtered.body}</p>
+        <div class="card-bottom">
+          <button class="up-vote"></button>
+          <button class="down-vote icon"></button>
+          <p>quality: <span id="vote">${filtered.quality}</span></p>
+        </div>
+        <hr>
+      </article>
+    `)
+}
+
+$('.search-input').on('input', function() {
+  var searchResult = $(this).val().toUpperCase();
+              console.log(searchResult)
+              console.log(ideaArray);
+  var results = ideaArray.filter(function(idea) {
+   return idea.title.toUpperCase().includes(searchResult) || idea.body.toUpperCase().includes(searchResult)
+  })
+  console.log(results)
+  $('.idea-container').empty();
+ results.forEach(function(result){
+   prependExistingCard(result);
+ })
+})
+
+
+// //Search Bar Function
+// function searchTitle() {
+//   var inputText = $('.search-input').val();
+//   console.log(inputText)
+//   if (inputText) {
+//     $('.idea-card').find("h2:not(:contains(" + inputText + "))").closest('.idea-card').slideUp();
+//     $('.idea-card').find("h2:contains(" + inputText + ")").slideDown()
+// } else {
+//   $('.idea-card').slideDown();
+//   }
+// };
+//
+// function searchBody() {
+//     var inputText = $('.search-input').val();
+//     if (inputText) {
+//     $('.idea-card').find(".idea-text:not(:contains(" + inputText + "))").closest('.idea-card').slideUp();
+//     $('.idea-card').find(".idea-text:contains(" + inputText + ")").slideDown();
+//   } else {
+//     $('.idea-card').slideDown();
+//   }
+// };
 
 //enable the save button function
 function enableSaveButton()  {
@@ -257,42 +251,3 @@ function editBody() {
   })
   addToLocal();
 }
-//*********************************************************************
-//******************  The Graveyard of Failed Ideas *******************
-//*********************************************************************
-
-
-//failed down-vote click
-// $('.idea-container').on('click', '.down-vote', function() {
-//   counter --;
-//   if(counter <= 0) {
-//     $('.down-vote').prop("disabled", true);
-//   }
-//   else if (counter > 0) {
-//     $('.down-vote').prop("disabled", false)
-//   }
-//   console.log(counter)
-// });
-
-//failed attempt to save upvote
-// function saveUpvote(){
-//   var uniqueQuality = document.querySelector('.quality');
-//   var uniqueTitle = document.querySelector('h2')
-//   var uniqueBody = document.querySelector('.idea-tet')
-//   console.log($(uniqueQuality).text());
-
-  // ideaArray.forEach(function(idea, index){
-  //   if (idea.quality === "swill"){
-  //     $(uniqueQuality).text('plausible');
-  //   }if (idea.quality === 'plausible'){
-  //     $(uniqueQuality).text('genius');
-  //   }
-  // })
-// };
-
-// // failed idea to check Storage on page load
-// function checkStorage () {
-//   var stringifiedArr = localStorage.getItem(ideaArray);
-//   ideaArray = JSON.parse(stringifiedArr) || [];
-//   console.log('check storage function: ' + ideaArray);
-// }
